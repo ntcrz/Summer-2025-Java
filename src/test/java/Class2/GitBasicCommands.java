07cbf807f42e21a63bb38c001212b4e869173c4d
Anita Cruz <ntcrz@hotmail.com>
2025-05-11 13:05:43 -0400

    Update with Class 2

---
+++ src/test/java/Class2/GitBasicCommands.java
@@ -0,0 +1,20 @@
+package Class2;
+
+public class GitBasicCommands {
+    /**
+     * git status: to check the different between local-repo and remote-repo
+     *
+     * To send files/code into remote repo
+     * 1. check the updates using git-status command
+     * 2. add the files/folder that you want to send to remote-repo
+     *      git add filename1 filename2 filename3
+     *          OR
+     *      git add filename1
+     *      git add filename2
+     *      git add filename3
+     * 3. add a commit-message
+     *      git commit -m "Enter your commit-message"
+     * 4. to actually send/push committed files/folder into remote-repo
+     *      git push
+     */
+}
